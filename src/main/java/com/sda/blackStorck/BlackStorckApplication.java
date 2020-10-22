666970da174df8ea4badb977bac93efda566f902
Ola <aleksandra.ljs@gmail.com>
2020-10-22 20:21:36 +0200

    empty springboot app

---
+++ src/main/java/com/sda/blackStorck/BlackStorckApplication.java
@@ -0,0 +1,13 @@
+package com.sda.blackStorck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackStorckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackStorckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/blackStorck/BlackStorckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.blackStorck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackStorckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
